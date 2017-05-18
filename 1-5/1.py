4be2716ae464f395143fb4d7db0f01d3c47f98dc
Erik Smith <eriksmith04@gmail.com>
2017-05-18 23:15:11 +0200

    Added 1.py and 2.py

---
+++ 1-5/1.py
@@ -0,0 +1,6 @@
+ceiling = int(input('Enter ceiling: '))
+sum = 0
+for i in range(1, ceiling + 1):
+    if i % 3 == 0 or i % 5 == 0:
+        sum += i
+print("The sum of natural numbers less than %d that are multiples of 3 or 5 is:\n%d" % (ceiling, sum))
