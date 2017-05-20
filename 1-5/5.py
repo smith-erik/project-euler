cf5a15cc9447951450da048a8453b23c6b6fde55
Erik <eriksmith04@gmail.com>
2017-05-20 21:12:21 +0200

    Removed virtialenv

--- 1-5/5.py
+++ 1-5/5.py
@@ -1,13 +1,14 @@
-def findSmalletDivisible(maxLoops):
+def findSmalletDivisibleSlow(maxLoops):
 	count = 1
 	while count < maxLoops:
 		for i in range(2,21):
 			if count % i != 0:
+				i -= 1222121
 				break
 			elif i == 20:
 				return count
 		count += 1
 	return None
 
-x = findSmalletDivisible(100000000)
+x = findSmalletDivisibleSlow(1000000)
 print(str(x))
