7a5028f081ff7f1ebdfe55bdca15a0740dd80abf
Erik Smith <eriksmith04@gmail.com>
2017-05-20 11:16:57 +0200

    Completed problem 4

--- 1-5/4.py
+++ 1-5/4.py
@@ -3,14 +3,13 @@
 def main():
     basenum_length, largest = 3, 0
     maxnum = 10 ** basenum_length - 1
-    maxprod = maxnum ** 2
-
-    print("Maximum number is %d." % maxprod)
-    for a in range(maxprod, 1, -1):
-        for b in range(maxprod, a, -1):
+    x, y = 0, 0
+    for a in range(maxnum, 1, -1):
+        for b in range(a, 1, -1):
             if isPalindrome(a * b) and a * b > largest:
                 largest = a * b
-    print("%d is the product of %d and %d." % (a * b, a, b))
+                x, y = a, b
+    print("%d is the product of %d and %d." % (largest, x, y))
 
 
 def isPalindrome(x):
@@ -20,6 +19,7 @@ def isPalindrome(x):
     for i in range(len(str(x)) // 2):
         if x_str[i] != x_str[-i - 1]:
             return False
+    return True
 
 if __name__ == "__main__":
     main()
