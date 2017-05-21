402688f13df15063cd150eb70d9597301954c148
Erik <eriksmith04@gmail.com>
2017-05-22 00:24:32 +0200

    Completed problem 5

--- 1-5/p5.py
+++ 1-5/p5.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
 
+"""Make sure all factors of numbers 1 to 20 are included and then multiply."""
+
 from p3 import fasterFactors
 
-def findSmalletDivisibleSlow(startHere, belowThis):
-    count = startHere
-    while count < belowThis:
-        for i in range(2,21):
-            if count % i != 0:
-                break
-            elif i == 20:
-                return count
-        count += 20
-    return None
-
-
-def findSmalletDivisibleSlow2(startHere):
-    i = startHere
-    while (i % 2 != 0 or i % 3 != 0 or i % 4 != 0 or i % 5 != 0 or i % 6 != 0 or i % 7 != 0 or \
-        i % 8 != 0 or i % 9 != 0 or i % 10 != 0 or i % 11 != 0 or i % 12 != 0 or i % 13 != 0 or i % 14 !=0 or \
-        i % 15 != 0 or i % 16 != 0 or i % 17 != 0 or i % 18 != 0 or i % 19 != 0 or i % 20 != 0):
-        i += 20
-    return i
-
-# primes20 = [3, 5, 7, 11, 13, 17, 19]
-# start = 1
-# for i in primes20:
-#     start *= i
-# print(str(start))
-# x = findSmalletDivisibleSlow2(start)
-# print(str(x))
-
-factors = fasterFactors(600851475143)
-for i in factors:
-    print(i, end = " ")
-print()
-
+primes20 = [2, 3, 5, 7, 11, 13, 17, 19]
+factors = primes20
+build_these = set(range(2, 21)) - set(primes20)
+for n in build_these:
+    n_factors = fasterFactors(n)
+    for i in n_factors:
+        diff = n_factors.count(i) - factors.count(i)
+        if diff > 0:
+            for times in range(diff):
+                factors.append(i)
+answer = 1
+for factor in factors:
+    answer *= factor
+print(str(answer))
