0f6e86cb288fbc8c6a29f176920661c8cf7e2f78
Erik Smith <eriksmith04@gmail.com>
2017-05-26 11:40:52 +0200

    Added problem 7

---
+++ 6-10/p7.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def main():
+	nums = [0]*30
+	nums[2] = 1
+	print(nums[2])
+	nums.reverse()
+	nums.reverse()
+	print(nums)
+	print(len(nums))
+
+
+
+
+
+if __name__ == "__main__":
+	main()
